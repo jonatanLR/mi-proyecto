1bde6b56034f1ed0505b31bdf4ec80cfd2925ca3
jonatanLR <jonatan.lopezr67@gmail.com>
2020-09-08 00:45:31 -0600

    mi primer coomit

--- App.js
+++ App.js
@@ -1,21 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// // import { StatusBar } from 'expo-status-bar';
+// import React from 'react';
+// import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+// // export default function App() {
+// //   return (
+// //     <View style={styles.container}>
+// //       <Text>Open up App.js to start working on your app!</Text>
+// //       <StatusBar style="auto" />
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// //     </View>
+// //   );
+// // }
+
+// export default class App extends React.Component{
+// 	render() {
+// 		return(
+// 			<View style={styles.container}>
+// 					<Text>Hola mundo</Text>
+// 			</View>
+// 		);
+// 	}
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
+//--------------------------------------------------------
+
+
